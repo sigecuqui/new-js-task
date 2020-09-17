3f9d1d5fb80c1b26cf96169966b908d021a99ae6
sigecuqui <sigita.guogaite@gmail.com>
2020-09-17 11:09:16 +0300

    suruke ciges

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-const pack = {
-    amount: 20,
-    smoke() {
-        this.amount--;
-        return this.amount;
-    }
-}
 
 const container = document.querySelector('body');
-const smoking = document.createElement('button');
-container.append(smoking);
-smoking.innerText = pack.amount;
-smoking.addEventListener('click', () => {
-    if (pack.amount > 0) {
-        smoking.innerText = pack.smoke();
-    }
-})
-
-function letsDavaiSmoke(object) {
-    console.log(object.amount);
-    smoking.innerText = object.amount;
-}
-
-letsDavaiSmoke(pack);
 
 const studentai = ['Bzegosz', 'Boleslov', 'Czeslov',];
 
 studentai.forEach((value, index) => {
-    const naujasPack = {...pack};
+    const pack = {
+        amount: 20,
+        smoke() {
+            this.amount--;
+            return this.amount;
+        }
+    }
+    const smoking = document.createElement('button');
+    container.append(smoking);
+    smoking.innerText = `${value}: ${pack.amount}`;
+    smoking.addEventListener('click', () => {
+        if (pack.amount > 0) {
+            smoking.innerText = `${value}: ${pack.smoke()}`;
+        }
+    })
+
+    function letsDavaiSmoke(object) {
+        console.log(object.amount);
+        smoking.innerText = `${value}: ${object.amount}`;
+    }
+    letsDavaiSmoke(pack);
     studentai[index] = {
         vardas: value,
-        pakelis: naujasPack
+        pakelis: {...pack}
     };
 })
 
